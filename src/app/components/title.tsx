e5c7e755df2fd5d9f325187057ca5c423c38a6ed
barbz <eduardob907@gmail.com>
2023-10-31 21:25:01 +0100

    start designing main page

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -1,11 +1,11 @@
 export default function Title() {
     return (
         <div>
-            <a className="fullname">Eduardo &apos;barbz&apos; Barbosa</a>
+            <a href="/" className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Eduardo &apos;barbz&apos; Barbosa</a>
             <br/>
-            <a className="job opacity-60 text-teal-600">Backend Engineer</a>
+            <a className="text-teal-300 mt-3 text-lg font-medium tracking-tight sm:text-xl">Backend Engineer at Adevinta</a>
             <br/>
-            <small className="silenced-text text-slate-500">&quot;Typical backend engineer that can do frontend stuff with the help of Stackoverflow&quot;</small>
+            <small className="silenced-text text-slate-500 mt-4 max-w-xs leading-normal">&quot;Typical backend engineer that can do frontend stuff with the help of Stackoverflow&quot;</small>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,19 @@ import Title from "@/app/components/title";
 
 export default function Home() {
     return (
-        <main
-            className="flex flex-row items-center justify-between min-h-screen max-w-screen-xl mx-auto px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
-            <Title/>
-            <Social/>
-        </main>
+        <body
+            className="bg-grey-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+        <div
+            className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+            <div className="lg:flex lg:justify-between lg:gap-4">
+                <header
+                    className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+                    <Title/>
+                    <Social/>
+                </header>
+            </div>
+        </div>
+        </body>
+
     )
 }
