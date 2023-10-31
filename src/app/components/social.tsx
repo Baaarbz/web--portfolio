db3cbbc753b5940e005d7f6ae8edfb5cf28a5799
barbz <eduardob907@gmail.com>
2023-10-31 20:24:12 +0100

    initial commit :rocket

---
+++ src/app/components/social.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+export default function Social() {
+    function openGithubProfile() {
+        location.href = 'https://github.com/baaarbz'
+    }
+
+    function openLinkedinProfile() {
+        location.href = 'https://www.linkedin.com/in/eduardobarbosatarrio'
+    }
+
+    function openSendMail() {
+        location.href = 'mailto:eduardo.barbosa.tarrio@gmail.com'
+    }
+
+    return (
+        <div className="social-container">
+            <button className="social-btn" onClick={openGithubProfile}>
+            <span className="social-icon-container">
+            <Github/>
+            </span>
+                <span className="social-icon-bg github-icon"></span>
+            </button>
+            <button className="social-btn" onClick={openLinkedinProfile}>
+            <span className="social-icon-container">
+            <Linkedin/>
+            </span>
+                <span className="social-icon-bg linkedin-icon"></span>
+            </button>
+            <button className="social-btn" onClick={openSendMail}>
+            <span className="social-icon-container">
+            <Mail/>
+            </span>
+                <span className="social-icon-bg mail-icon"></span>
+            </button>
+        </div>
+    )
+}
+
+function Github() {
+    return (
+        <svg fill="white" viewBox="0 0 496 512" height="1.6em">
+            <path
+                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
+        </svg>
+    )
+}
+
+function Linkedin() {
+    return (
+        <svg fill="white" viewBox="0 0 24 24" height="1.6em">
+            <path
+                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+        </svg>
+    )
+}
+
+function Mail() {
+    return (
+        <svg fill="white" viewBox="0 0 24 24" height="1.6em">
+            <path
+                d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/>
+        </svg>
+    )
+}
---
+++ src/app/components/title.tsx
@@ -0,0 +1,11 @@
+export default function Title() {
+    return (
+        <div>
+            <a className="fullname">Eduardo &apos;barbz&apos; Barbosa</a>
+            <br/>
+            <a className="job opacity-60 text-teal-600">Backend Engineer</a>
+            <br/>
+            <small className="silenced-text text-slate-500">&quot;Typical backend engineer that can do frontend stuff with the help of Stackoverflow&quot;</small>
+        </div>
+    )
+}
