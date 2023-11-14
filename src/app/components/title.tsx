c77177dafe39995ca28c366a936247e93a16b1c0
barbz <eduardob907@gmail.com>
2023-11-14 14:48:04 +0100

    add h1 header

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -1,8 +1,8 @@
 export default function Title() {
     return (
         <div className="max-w-lg mx-auto">
-            <a href="/"
-               className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Eduardo Barbosa</a>
+            <h1
+               className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Eduardo Barbosa</h1>
             <div className="text-teal-300 mt-3 text-lg font-medium tracking-tight sm:text-xl">Backend Engineer at
                 Adevinta</div>
             <div className="text-slate-500 mt-4 leading-normal text-opacity-80">&quot;Typical backend
